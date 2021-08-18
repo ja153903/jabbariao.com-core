ebcf3aa45747868d1a922b54aedf7a01e418705e
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-08-17 23:50:35 -0400

    Creating base API

--- jabbariao_me_core/urls.py
+++ jabbariao_me_core/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .apps.posts.api import PostViewSet
+
+router = DefaultRouter()
+router.register(r"posts", PostViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("", include(router.urls)),
 ]
