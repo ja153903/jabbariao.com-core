801c320940274274fee05bb1fcc77bcb2e633fda
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-08-17 23:56:02 -0400

    Formatting

--- jabbariao_me_core/asgi.py
+++ jabbariao_me_core/asgi.py
@@ -1,16 +1,7 @@
-"""
-ASGI config for jabbariao_me_core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jabbariao_me_core.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jabbariao_me_core.settings")
 
 application = get_asgi_application()
