cb22ebcc21ff22cc0f805c532a96b79be8699d58
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-08-17 23:53:47 -0400

    Applying formatting

--- jabbariao_me_core/settings/utils.py
+++ jabbariao_me_core/settings/utils.py
@@ -8,13 +8,13 @@ load_dotenv()
 
 def get_database_credentials() -> Dict[str, str]:
     return {
-        'NAME': os.getenv('SUPABASE_NAME'),
-        'USER': os.getenv('SUPABASE_USER'),
-        'PASSWORD': os.getenv('SUPABASE_PASSWORD'),
-        'HOST': os.getenv('SUPABASE_HOST'),
-        'PORT': os.getenv('SUPABASE_PORT')
+        "NAME": os.getenv("SUPABASE_NAME"),
+        "USER": os.getenv("SUPABASE_USER"),
+        "PASSWORD": os.getenv("SUPABASE_PASSWORD"),
+        "HOST": os.getenv("SUPABASE_HOST"),
+        "PORT": os.getenv("SUPABASE_PORT"),
     }
 
 
 def get_django_secret_key() -> str:
-    return os.getenv('DJANGO_SECRET_KEY')
+    return os.getenv("DJANGO_SECRET_KEY")
