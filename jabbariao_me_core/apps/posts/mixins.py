d023100e3923211e507f4516e41f9111da9412c1
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-08-23 00:22:16 -0400

    Adding some docstring to explain this logic

--- jabbariao_me_core/apps/posts/mixins.py
+++ jabbariao_me_core/apps/posts/mixins.py
@@ -20,6 +20,19 @@ class PostTagMixin:
         logger.info(f"[PostTagMixin::add_tags_to_post]: Added {len(tags)} tags ({tags}) to post with id # {post.id}")
 
     def update_tags_for_post(self, post: Post, tags: List[str]) -> None:
+        """
+        This function takes a Post instance and a list of tags.
+        This list of tags comes from an HTTP request body.
+        To update the tags for a post, we take the set difference
+        of the tags that we got from the request from the tags
+        that exist on the post instance.
+
+        We then add the intersection from those two sets to the
+        updated set of tags.
+
+        We then clear the current tags on the post and use the
+        function above to add the updated tags to the post.
+        """
         if not tags:
             return
 
@@ -29,5 +42,7 @@ class PostTagMixin:
         # (B - A) + (B ^ A)
         updated_tags = tags.difference(current_tags)
         updated_tags.update(tags.intersection(current_tags))
+
+        post.tags.clear()
 
         self.add_tags_to_post(post, list(updated_tags))
