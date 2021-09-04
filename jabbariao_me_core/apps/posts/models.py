eb1f90f2b2d326729df991688852beadb95a8cc0
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-09-04 11:26:34 -0400

    Adding new migration for Comments

--- jabbariao_me_core/apps/posts/models.py
+++ jabbariao_me_core/apps/posts/models.py
@@ -11,3 +11,12 @@ class Post(models.Model):
 class Tag(models.Model):
     name = models.CharField(blank=True, default="", max_length=256, primary_key=True)
     posts = models.ManyToManyField(Post, related_name="tags")
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    # Note here that for replies, we have to make sure that we can still render old replies
+    parent = models.ForeignKey("self", null=True, related_name="replies", on_delete=models.DO_NOTHING)
+    content = models.CharField(blank=True, default="", max_length=140)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
